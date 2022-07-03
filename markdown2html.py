0cb6050b9cc2ff8cdaf37ff156a8dcd1d8f3d6a8
root <root@DESKTOP-G4O80SJ.localdomain>
2022-07-03 23:37:26 +0300

    modify

--- markdown2html.py
+++ markdown2html.py
@@ -1,15 +1,27 @@
 #!/usr/bin/python3
+"""
+takes two argumnets
+"""
 import sys
 import os
+import markdown
 
-if __name__ == "__main__":
-
+if __name__ == '__main__':
+    """ this function takes two arguments markdown and htmlfile"""
     if(len(sys.argv) < 3):
-        print("Usage: ./markdown2html.py README.md README.html" ,file=sys.stderr)
-        exit (1)
-    
-    markdown = sys.argv[1]
-    markdownfile = os.path.exists(markdown)
+        print("Usage: ./markdown2html.py README.md README.html", file=sys.stderr)
+        exit(1)
+    markdownf = sys.argv[1]
+    markdownfile = os.path.exists(markdownf)
     if(markdownfile == False):
-        print("missing {}".format(markdown))
-        exit (1)
+        print(f'missing {markdownf}', file=sys.stderr)
+        exit(1)
+
+    with open(sys.argv[1], 'r') as f:
+        text = f.read()
+        html = markdown.markdown(text)
+
+    with open(sys.argv[2], 'w') as f:
+        f.write(html)
+
+
