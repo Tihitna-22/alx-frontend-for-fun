7ed2cd533e9b078d4e51e9c622ba433d33086806
Tihitna <71766297+Tihitna-22@users.noreply.github.com>
2022-07-03 21:58:48 +0300

    Update markdown2html.py

--- markdown2html.py
+++ markdown2html.py
@@ -2,13 +2,14 @@
 import sys
 import os
 
+if __name__ == "__main__":
 
-if(len(sys.argv) < 3):
-    print("Usage: ./markdown2html.py README.md README.html" ,file=sys.stderr)
-    exit (1)
-   
-markdown = sys.argv[1]
-markdownfile = os.path.exists(markdown)
-if(markdownfile == False):
-    print("missing {}".format(markdown))
-    exit (1)
+    if(len(sys.argv) < 3):
+        print("Usage: ./markdown2html.py README.md README.html" ,file=sys.stderr)
+        exit (1)
+    
+    markdown = sys.argv[1]
+    markdownfile = os.path.exists(markdown)
+    if(markdownfile == False):
+        print("missing {}".format(markdown))
+        exit (1)
