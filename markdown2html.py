df2d8357f4304e840b1394a09c38a540697529ac
root <root@DESKTOP-G4O80SJ.localdomain>
2022-07-03 21:30:55 +0300

    modify

--- markdown2html.py
+++ markdown2html.py
@@ -6,9 +6,9 @@ import os
 if(len(sys.argv) < 3):
     print("Usage: ./markdown2html.py README.md README.html" ,file=sys.stderr)
     exit (1)
-if(sys.argv[1] == "README.md"):    
-    markdown = sys.argv[1]
-    markdownfile = os.path.exists(markdown)
-    if(markdownfile == False):
-        print("missing README.md")
-        exit (1)
+   
+markdown = sys.argv[1]
+markdownfile = os.path.exists(markdown)
+if(markdownfile == False):
+    print("missing {}".format(markdown))
+    exit (1)
